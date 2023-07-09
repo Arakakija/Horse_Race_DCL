af0489df789dd1d3e0b53fb002f694a59eba31e8
Mija <mijaelcohen@gmail.com>
2023-07-09 20:24:24 -0300

    added dockerfile

--- src/ui.tsx
+++ src/ui.tsx
@@ -7,16 +7,19 @@ const uiComponent = () => (
     uiTransform = {{
       width:'100%',
       height: '100%',
+      flexDirection:"row",
+      alignItems:"center",
       margin: { top: '0%', left: '0%'}
     }}
     uiBackground={{ color: Color4.Clear() }}
   >
     <UiEntity
         uiTransform={{
-            width: 100,
+            width: 400,
             height: 100,
-            margin: { top: '0%', left: '40%' }
+            margin: { top: '10px'}
         }}
+        uiBackground={{ color: Color4.create(0, 0, 0, 0.6) }}
         uiText={{ value: gameStatus + timeToWait , fontSize: 30 }}
     />
 
