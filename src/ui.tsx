5f4a072695518fff347e487165950db187b5e853
milruby <138411925+milruby@users.noreply.github.com>
2023-07-10 21:53:03 -0300

    UI Style + New Assets

--- src/ui.tsx
+++ src/ui.tsx
@@ -14,50 +14,62 @@ function selectOption(index: number) {
   const uiComponent = () => (
     <UiEntity
       uiTransform={{
-        width: 150,
-        height: 250,
+        width: 250,
+        height: 320,
         //  { top: 16, right: 0, bottom: 8 left: 270 },
-        margin: '200px 0 8px 20px',
+        margin: '16% 0 0 1%',
         // { top: 4, bottom: 4, left: 4, right: 4 },
         padding: 4,
       }}
-      uiBackground={{ color: Color4.create(0.5, 0.8, 0.1, 0.6) }}
+      uiBackground={{
+        textureMode: 'center',
+        texture: {
+        src: "images/under-the-sea.png"}
+      }}
     >
       <UiEntity
         uiTransform={{
-          width: '500%',
+          width: '100%',
           height: '100%',
           flexDirection: 'column',
           alignItems: 'center',
-          justifyContent: 'space-between'
+          justifyContent: 'space-between',
+          padding: {top:24, right:16, bottom:16, left:16},
         }}
-        uiBackground={{ color: Color4.fromHexString("#70ac76ff") }}
+        uiBackground={{
+          textureMode: 'center',
+          texture: {
+          src: "images/under-the-sea.png"}
+        }}
       >
       <UiEntity
         uiTransform={{
           width: '100%',
-          height: '50px',
+          height: '50%',
           flexDirection: 'row',
           alignItems: 'center',
           justifyContent: 'space-between',
-          margin : {bottom: "10px"}
+          margin : {top: "24px", bottom: "24px"}
         }}
         uiBackground={{ color: Color4.Clear()}}
       >
         <Label
-          value = {"Select Horse: "}
+          value = {"Pick Horse:"}
           uiTransform={{
-            margin : {bottom: "10px", left : "30px"}
+            width: '100%',
           }}
+          fontSize={16}
+          textAlign="middle-center"
         ></Label>
+
       <Dropdown
         options= {[`Yellow`, `Purple`, `Green`,' Pink']}
-        fontSize={8}
+        fontSize={16}
         onChange={selectOption}
         uiTransform={{
-          width: "50%",
+          width: "100%",
           height: "40px",
-          margin : {right : "5px"}
+          margin : {left : "12px"}
         }}
         uiBackground={{color: Color4.White()}}
       />
@@ -75,50 +87,49 @@ function selectOption(index: number) {
         uiBackground={{ color: Color4.Clear()}}
       >
         <Label
-          value = {"Amount To Bet: "}
+          value = {"Bet Amount: "}
           uiTransform={{
-            width : "10%",
-            margin : {bottom: "20px", left : "35px"}
-          }}
-          fontSize={9.5}
+              width: '100%',
+            }}
+            fontSize={16}
+            textAlign="middle-center"
         ></Label>
       <Input
         uiTransform={{
-          width : "40%",
-          height: '30px',
-          margin : {top: "5px", right : "10px"}
+          width: "100%",
+          height: "40px",
+          margin : {left : "12px"}
         }}
         onChange={(e) =>{ amount = parseInt(e) }}
-        fontSize={9}
-        placeholder={""}
+        fontSize={16}
+        placeholder={"Your bet"}
         placeholderColor={Color4.Gray()}
         uiBackground={{ color: Color4.White()}}
       />
       </UiEntity>
-
       <Label
-          value = {"Cash: " + playerCash}
+          value = {"Coins: " + playerCash}
           uiTransform={{
-            width : "10%",
-            margin : {bottom: "50px", left : "35px"}
+            width : "100%",
+            height: 16,
+            margin: 24
           }}
           fontSize={20}
         ></Label>
-
       <Button
-          uiTransform={{ width: 100, height: 40, margin: 8 }}
+          uiTransform={{ width: "100%", height: 100, margin: 6 }}
           value='Place Bet'
           variant='primary'
-          fontSize={14}
+          fontSize={16}
           onMouseDown={() => {
             placeBet(amount)
           }}
         />
         <Button
-          uiTransform={{ width: 100, height: 40, margin: 8 }}
+          uiTransform={{ width: "100%", height: 100, margin: 6 }}
           value='Start Game'
           variant='primary'
-          fontSize={14}
+          fontSize={16}
           onMouseDown={() => {
           }}
         />
